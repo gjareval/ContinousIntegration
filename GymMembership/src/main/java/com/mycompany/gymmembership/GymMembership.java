f0902995d23f8e692ba7e2239034c51366605bb7
Cliente CONTROL-PC <CltControl@GGIECSOFWRK154.espol.edu.ec>
2024-06-26 13:52:53 -0500

    Project

---
+++ GymMembership/src/main/java/com/mycompany/gymmembership/GymMembership.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gymmembership;
+
+/**
+ *
+ * @author CltControl
+ */
+public class GymMembership {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
